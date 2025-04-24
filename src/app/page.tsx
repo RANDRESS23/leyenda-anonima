a289e6086a1e61f8bfeb68853ab1fffb53f167a2
Raúl Quimbaya <raulquimbaya4@gmail.com>
2025-04-23 21:55:21 -0500

    feat: refactor Home component layout and enhance footer with links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,46 @@
+import { TypingAnimation } from "@/components/magicui/typing-animation";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className="max-w-5xl mx-auto py-10 px-4 flex gap-x-5 min-h-screen justify-center">
+      <div className="flex flex-col justify-center">
+        <header className="my-8 text-white">
+          <div className="flex items-center gap-x-2 h-20">
+            <span className="text-2xl">📺</span><TypingAnimation>Canal 0x23 - Leyenda Anónima</TypingAnimation>
+          </div>
+          <p className="mb-3 text-[#888] md:-mt-4">Transmisión capturada. Verificando integridad...</p>
+        </header>
+        <div className="flex justify-center items-center md:hidden mb-7">
+          <Image src="/images/leyenda-anonima.webp" width={150} height={150} alt="Imagen estilo televisón CRT" className="transition-all duration-500 hover:shadow-[0_0_60px_#60cc8ae6] rounded-xl rotate-3 glitch-image-animation" />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <section className="text-white flex flex-col items-start">
+          <span className="text-xl text-balance">La red guarda <span className="text-green-300">secretos</span>. Algunos están <span className="text-green-300">ocultos</span>, otros solo esperan ser <span className="text-green-300">descubiertos</span>.</span>
+          <button className="text-lg mt-8 bg-black border border-[#333] hover:bg-white transition px-4 py-2 text-white hover:text-black cursor-pointer animate-[floatGlitch_3s_infinite] hover:animate-[glitchEffect_0.5s_infinite] duration-300 shadow-lg hover:shadow-[0_0_20px_#00ffcc]">
+            <div className="flex items-center justify-center font-bold gap-x-2 text-base sm:text-xl">
+              Desbloquear transmisión
+            </div>
+          </button>
+        </section>
+        <footer className="flex flex-col justify-between items-start pt-24 text-white">
+          <div className="flex flex-col md:flex-row space-x-4">
+            <span>Hecho con <span className="text-red-400">❤</span> por Raúl Quimbaya</span>
+            <span className="hidden md:flex justify-center items-center">-</span>
+            <div className="flex space-x-2 md:space-x-4">
+              <Link href="https://www.linkedin.com/in/raul-quimbaya/" target="_blank" rel="noopener noreferrer" className="hover:text-blue-300 transition-colors text-blue-200">LinkedIn</Link>
+              <Link href="https://github.com/RANDRESS23" target="_blank" rel="noopener noreferrer" className="hover:text-violet-300 transition-colors text-violet-200">GitHub</Link>
+            </div>
+          </div>
+          <div className="flex space-x-2 text-sm text-[#888]">
+            <span>Diseño inspirado en</span>
+            <Link href="https://codember.dev/" target="_blank" rel="noopener noreferrer" className="hover:text-yellow-300/70 transition-colors text-yellow-200/70 underline">Codember</Link>
+          </div>
+        </footer>
+      </div>
+      <div className="hidden md:flex md:justify-center md:items-center">
+        <Image src="/images/leyenda-anonima.webp" width={600} height={600} alt="Imagen estilo televisón CRT" className="transition-all duration-500 hover:shadow-[0_0_60px_#60cc8ae6] rounded-xl rotate-3 glitch-image-animation" />
+      </div>
+    </main>
   );
 }
